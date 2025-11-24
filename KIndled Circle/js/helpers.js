a88113624f04d5869238a376a5699b690fd171e2
avesatn-tech <avesatn@gmail.com>
2025-11-24 12:41:03 +0700

    Update helpers.js

--- KIndled Circle/js/helpers.js
+++ KIndled Circle/js/helpers.js
@@ -1,19 +1,6 @@
-// helpers.js
-// Небольшие утилитарные функции.
-
-export function escapeHtml(str) {
-  return String(str)
-    .replace(/&/g, "&amp;")
-    .replace(/</g, "&lt;")
-    .replace(/>/g, "&gt;")
-    .replace(/"/g, "&quot;")
-    .replace(/'/g, "&#039;");
-}
-
-export function qs(sel, ctx = document) {
-  return ctx.querySelector(sel);
-}
-
-export function qsa(sel, ctx = document) {
-  return Array.from(ctx.querySelectorAll(sel));
+// js/helpers.js
+export function qs(sel, ctx=document){ return ctx.querySelector(sel) }
+export function qsa(sel, ctx=document){ return Array.from(ctx.querySelectorAll(sel)) }
+export function escapeHtml(str){
+  return String(str).replace(/[&<>"']/g, s => ({'&':'&amp;','<':'&lt;','>':'&gt;','"':'&quot;',"'":"&#039;"}[s]));
 }
