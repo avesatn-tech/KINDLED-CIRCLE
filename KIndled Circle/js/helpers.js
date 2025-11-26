58c430ca7c6d519fd87c0c716fdf710fdbd716d1
avesatn-tech <avesatn@gmail.com>
2025-11-26 08:38:47 +0700

    Delete KIndled Circle/js/helpers.js

--- KIndled Circle/js/helpers.js
+++
@@ -1,6 +0,0 @@
-// js/helpers.js
-export function qs(sel, ctx=document){ return ctx.querySelector(sel) }
-export function qsa(sel, ctx=document){ return Array.from(ctx.querySelectorAll(sel)) }
-export function escapeHtml(str){
-  return String(str).replace(/[&<>"']/g, s => ({'&':'&amp;','<':'&lt;','>':'&gt;','"':'&quot;',"'":"&#039;"}[s]));
-}
